26693e812a207b15ce844f30c562468d1025695e
alexVpenev <alexVpenev+github@proton.me>
2024-02-24 20:53:32 +0100

    initialized spring boot maven project

---
+++ src/main/java/com/automating/k8s/k8sjenkinsawsautomationapp/K8sJenkinsAwsAutomationAppApplication.java
@@ -0,0 +1,13 @@
+package com.automating.k8s.k8sjenkinsawsautomationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sJenkinsAwsAutomationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sJenkinsAwsAutomationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automating/k8s/k8sjenkinsawsautomationapp/K8sJenkinsAwsAutomationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automating.k8s.k8sjenkinsawsautomationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sJenkinsAwsAutomationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
